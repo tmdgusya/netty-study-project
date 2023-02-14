1735466d70a86e330f9442455faf4042fadae5c3
정승현 <tmdgusya@naver.com>
2023-02-14 20:31:03 +0900

    DiscardServer 제작

---
+++ src/main/java/ex1/DiscardServerHandler.java
@@ -0,0 +1,24 @@
+package ex1;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = ((ByteBuf) msg);
+        while (in.isReadable()) {
+            System.out.println((char) in.readByte());
+            System.out.flush();
+        }
+        in.release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
